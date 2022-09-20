3640c51608eca02d01dff2895ce92077bf5d14ab
musyokamuasya <josephmuasya8@gmail.com>
2022-09-20 22:27:04 +0300

    Enum class for sort direction

---
+++ app/src/main/java/dev/ciox/ratiba/components/enums/SortDirection.kt
@@ -0,0 +1,15 @@
+package dev.ciox.ratiba.components.enums
+
+enum class SortDirection {
+    ASCENDING, DESCENDING;
+
+    companion object {
+        fun parse(s: String?): SortDirection {
+            return when (s) {
+                ASCENDING.toString() -> ASCENDING
+                DESCENDING.toString() -> DESCENDING
+                else -> throw IllegalStateException("Sort Direction must be ascending or descending")
+            }
+        }
+    }
+}
