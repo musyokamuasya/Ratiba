158eee75e3274ee3b01d6e503c62aeee0572ae8a
musyokamuasya <josephmuasya8@gmail.com>
2022-09-20 22:38:34 +0300

    App compart extension functions

--- app/src/main/java/dev/ciox/ratiba/components/extensions/android/AppCompartExtensions.kt
+++ app/src/main/java/dev/ciox/ratiba/components/extensions/android/AppCompartExtensions.kt
@@ -1,13 +1,47 @@
 package dev.ciox.ratiba.components.extensions.android
 
+import android.content.Context
 import android.content.Intent
-import android.os.Parcelable
-import dev.ciox.ratiba.components.extensions.jdk.toArrayList
+import android.os.Build
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 
-fun <T : Parcelable> Intent.putExtra(key: String, items: List<T>) {
-    putParcelableArrayListExtra(key, items.toArrayList())
+fun Context.startForegroundServiceCompat(service: Intent) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        startForegroundService(service)
+    else startService(service)
 }
 
-fun <T : Parcelable> Intent.getParcelableListExtra(key: String): List<T>? {
-    return getParcelableArrayListExtra<T>(key)?.toList()
+fun AppCompatActivity.createSnackbar(
+    @StringRes textRes: Int,
+    view: View = window.decorView.rootView,
+    duration: Int = Snackbar.LENGTH_SHORT
+): Snackbar {
+    return Snackbar.make(view, getString(textRes), duration).apply { show() }
+}
+
+fun AppCompatActivity.createToast(
+    @StringRes textRes: Int,
+    duration: Int = Toast.LENGTH_SHORT
+): Toast {
+    return Toast.makeText(this, getString(textRes), duration).apply { show() }
+}
+
+fun Fragment.createSnackbar(
+    @StringRes textRes: Int,
+    view: View = this.requireView(),
+    duration: Int = Snackbar.LENGTH_SHORT
+): Snackbar {
+    return Snackbar.make(view, getString(textRes), duration).apply { show() }
+}
+
+fun Fragment.createToast(
+    @StringRes textRes: Int,
+    duration: Int = Toast.LENGTH_SHORT
+): Toast {
+    return Toast.makeText(requireContext(), getString(textRes), duration).apply { show() }
 }
---
+++ app/src/main/java/dev/ciox/ratiba/components/extensions/android/IntentExtensions.kt
@@ -0,0 +1,13 @@
+package dev.ciox.ratiba.components.extensions.android
+
+import android.content.Intent
+import android.os.Parcelable
+import dev.ciox.ratiba.components.extensions.jdk.toArrayList
+
+fun <T : Parcelable> Intent.putExtra(key: String, items: List<T>) {
+    putParcelableArrayListExtra(key, items.toArrayList())
+}
+
+fun <T : Parcelable> Intent.getParcelableListExtra(key: String): List<T>? {
+    return getParcelableArrayListExtra<T>(key)?.toList()
+}
