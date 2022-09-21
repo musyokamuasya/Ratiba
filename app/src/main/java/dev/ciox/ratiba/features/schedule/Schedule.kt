baab141bd4a3f2c504aedd58489323105b6cd123
musyokamuasya <josephmuasya8@gmail.com>
2022-09-21 14:10:18 +0300

    Schedule data class implementation

--- app/src/main/java/dev/ciox/ratiba/features/schedule/Schedule.kt
+++ app/src/main/java/dev/ciox/ratiba/features/schedule/Schedule.kt
@@ -1,4 +1,255 @@
 package dev.ciox.ratiba.features.schedule
 
-class Schedule {
+import android.content.Context
+import android.os.Parcelable
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.DiffUtil
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.squareup.moshi.JsonClass
+import dev.ciox.ratiba.R
+import dev.ciox.ratiba.components.interfaces.Streamable
+import dev.ciox.ratiba.components.json.JsonDataStreamer
+import dev.ciox.ratiba.database.converter.DateTimeConverter
+import dev.ciox.ratiba.features.subject.Subject
+import kotlinx.android.parcel.Parcelize
+import okio.buffer
+import okio.sink
+import java.io.File
+import java.io.InputStream
+import java.time.*
+import java.util.*
+
+@Parcelize
+@JsonClass(generateAdapter = true)
+@Entity(
+    tableName = "schedules", foreignKeys = [ForeignKey(
+        entity = Subject::class,
+        parentColumns = arrayOf("subjectID"), childColumns = arrayOf("subject"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class Schedule @JvmOverloads constructor(
+    @PrimaryKey
+    var scheduleID: String = UUID.randomUUID().toString(),
+    var daysOfWeek: Int = 0,
+    var weeksOfMonth: Int = 0,
+    @TypeConverters(DateTimeConverter::class)
+    var startTime: LocalTime? = null,
+    @TypeConverters(DateTimeConverter::class)
+    var endTime: LocalTime? = null,
+    var subject: String? = null
+) : Parcelable, Streamable {
+
+    fun isToday(): Boolean {
+        val currentDate = LocalDate.now()
+
+        parseDaysOfWeek().forEach {
+            if (it == currentDate.dayOfWeek.value)
+                return@isToday true
+        }
+        return false
+    }
+
+    fun isTomorrow(): Boolean {
+        val currentDate = LocalDate.now()
+
+        parseDaysOfWeek().forEach {
+            if (it == currentDate.plusDays(1).dayOfWeek.value)
+                return@isTomorrow true
+        }
+        return false
+    }
+
+    fun format(context: Context, isAbbreviated: Boolean = false): String {
+        return StringBuilder().apply {
+            append(formatDaysOfWeek(context, isAbbreviated))
+            append(", ")
+            append(formatBothTime(context))
+        }.toString()
+    }
+
+    fun formatBothTime(context: Context): String {
+        return "${formatStartTime(context)} - ${formatEndTime(context)}"
+    }
+
+    fun formatStartTime(context: Context): String? {
+        return formatTime(context, startTime)
+    }
+
+    fun formatEndTime(context: Context): String? {
+        return formatTime(context, endTime)
+    }
+
+    /**
+     *   Function to format the daysOfWeek attribute
+     *   to human readable form
+     *   @param context used to fetch the appropriate string localization
+     *                  for the string resource id
+     *   @return the formatted days of week
+     *           (e.g. "Sunday, Monday and Thursday")
+     */
+    fun formatDaysOfWeek(context: Context, isAbbreviated: Boolean): String {
+        val builder = StringBuilder()
+        val list = parseDaysOfWeek()
+        list.forEachIndexed { index, i ->
+            // Append the appropriate day name string from string resource
+            val resID = if (isAbbreviated)
+                getStringResourceForDayAbbreviated(i)
+            else getStringResourceForDay(i)
+
+            builder.append(context.getString(resID))
+
+            // Check if the item's index is second to last,
+            // if it is, then add an "and" from string resource
+            // and if not, just append a comma
+            if (index == list.size - 2)
+                builder.append(context.getString(R.string.and))
+            else if (index < list.size - 2)
+                builder.append(", ")
+        }
+        return builder.toString()
+    }
+
+    @StringRes
+    fun getStringResourceForDayAbbreviated(day: Int): Int {
+        return when (day) {
+            DayOfWeek.SUNDAY.value -> R.string.days_of_week_item_sunday_short
+            DayOfWeek.MONDAY.value -> R.string.days_of_week_item_monday_short
+            DayOfWeek.TUESDAY.value -> R.string.days_of_week_item_tuesday_short
+            DayOfWeek.WEDNESDAY.value -> R.string.days_of_week_item_wednesday_short
+            DayOfWeek.THURSDAY.value -> R.string.days_of_week_item_thursday_short
+            DayOfWeek.FRIDAY.value -> R.string.days_of_week_item_friday_short
+            DayOfWeek.SATURDAY.value -> R.string.days_of_week_item_saturday_short
+            else -> 0
+        }
+    }
+
+    @StringRes
+    fun getStringResourceForDay(day: Int): Int {
+        return when (day) {
+            DayOfWeek.SUNDAY.value -> R.string.days_of_week_item_sunday
+            DayOfWeek.MONDAY.value -> R.string.days_of_week_item_monday
+            DayOfWeek.TUESDAY.value -> R.string.days_of_week_item_tuesday
+            DayOfWeek.WEDNESDAY.value -> R.string.days_of_week_item_wednesday
+            DayOfWeek.THURSDAY.value -> R.string.days_of_week_item_thursday
+            DayOfWeek.FRIDAY.value -> R.string.days_of_week_item_friday
+            DayOfWeek.SATURDAY.value -> R.string.days_of_week_item_saturday
+            else -> 0
+        }
+    }
+
+    fun parseDaysOfWeek(): List<Int> {
+        return Companion.parseDaysOfWeek(daysOfWeek)
+    }
+
+    fun hasWeek(week: Int): Boolean {
+        return weeksOfMonth and week == week
+    }
+
+    override fun toJsonString(): String? = JsonDataStreamer.encodeToJson(this, Schedule::class.java)
+
+    override fun toJsonFile(destination: File, name: String): File {
+        return File(destination, name).apply {
+            this.sink().buffer().use {
+                toJsonString()?.also { json -> it.write(json.toByteArray()) }
+            }
+        }
+    }
+
+    override fun fromInputStream(inputStream: InputStream) {
+        JsonDataStreamer.decodeOnceFromJson(inputStream, Schedule::class.java)?.also {
+            scheduleID = it.scheduleID
+            daysOfWeek = it.daysOfWeek
+            startTime = it.startTime
+            endTime = it.endTime
+            subject = it.subject
+        }
+    }
+
+    companion object {
+        const val BIT_VALUE_SUNDAY = 1
+        const val BIT_VALUE_MONDAY = 2
+        const val BIT_VALUE_TUESDAY = 4
+        const val BIT_VALUE_WEDNESDAY = 8
+        const val BIT_VALUE_THURSDAY = 16
+        const val BIT_VALUE_FRIDAY = 32
+        const val BIT_VALUE_SATURDAY = 64
+
+        const val BIT_VALUE_WEEK_ONE = 1
+        const val BIT_VALUE_WEEK_TWO = 2
+        const val BIT_VALUE_WEEK_THREE = 4
+        const val BIT_VALUE_WEEK_FOUR = 8
+
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Schedule>() {
+            override fun areItemsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem.scheduleID == newItem.scheduleID
+            }
+
+            override fun areContentsTheSame(oldItem: Schedule, newItem: Schedule): Boolean {
+                return oldItem.scheduleID == newItem.scheduleID
+                        && oldItem.startTime == newItem.startTime
+                        && oldItem.endTime == newItem.endTime
+                        && oldItem.daysOfWeek == newItem.daysOfWeek
+                        && oldItem.weeksOfMonth == newItem.weeksOfMonth
+                        && oldItem.subject == newItem.subject
+            }
+        }
+
+        fun formatTime(context: Context, time: LocalTime?): String? {
+            return time?.format(DateTimeConverter.getTimeFormatter(context))
+        }
+
+        fun parseDaysOfWeek(daysOfWeek: Int): List<Int> {
+            val days = mutableListOf<Int>()
+
+            if (daysOfWeek and 1 == BIT_VALUE_SUNDAY) days.add(DayOfWeek.SUNDAY.value)
+            if (daysOfWeek and 2 == BIT_VALUE_MONDAY) days.add(DayOfWeek.MONDAY.value)
+            if (daysOfWeek and 4 == BIT_VALUE_TUESDAY) days.add(DayOfWeek.TUESDAY.value)
+            if (daysOfWeek and 8 == BIT_VALUE_WEDNESDAY) days.add(DayOfWeek.WEDNESDAY.value)
+            if (daysOfWeek and 16 == BIT_VALUE_THURSDAY) days.add(DayOfWeek.THURSDAY.value)
+            if (daysOfWeek and 32 == BIT_VALUE_FRIDAY) days.add(DayOfWeek.FRIDAY.value)
+            if (daysOfWeek and 64 == BIT_VALUE_SATURDAY) days.add(DayOfWeek.SATURDAY.value)
+
+            return days
+        }
+
+        fun getNearestDateTime(day: Int, time: LocalTime): ZonedDateTime {
+            val currentDate = LocalDate.now().atStartOfDay(ZoneId.systemDefault())
+            val currentDayOfWeek = currentDate.dayOfWeek.value
+            var targetDay: Long = day.toLong()
+
+            if (day < currentDayOfWeek)
+                targetDay += 7
+
+            return currentDate.plusDays(targetDay - currentDayOfWeek)
+                .withHour(time.hour)
+                .withMinute(time.minute)
+                .withSecond(time.second)
+        }
+
+        fun getNextWeekDay(day: Int, time: LocalTime): ZonedDateTime? {
+            var currentDate = LocalDate.now().atStartOfDay(ZoneId.systemDefault())
+                .plusHours(time.hour.toLong())
+                .plusMinutes(time.minute.toLong())
+            if (currentDate.dayOfWeek.value >= day)
+                currentDate = currentDate.plusWeeks(1)
+            return currentDate.with(DayOfWeek.of(day))
+        }
+
+        fun toJsonFile(
+            items: List<Schedule>, destination: File,
+            name: String = Streamable.FILE_NAME_SCHEDULE
+        ): File {
+            return File(destination, name).apply {
+                this.sink().buffer().use {
+                    JsonDataStreamer.encodeToJson(items, Schedule::class.java)?.also { json ->
+                        it.write(json.toByteArray())
+                    }
+                }
+            }
+        }
+    }
 }
