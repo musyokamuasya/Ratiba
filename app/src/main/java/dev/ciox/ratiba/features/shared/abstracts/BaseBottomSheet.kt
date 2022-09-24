b2597f992aeb1b0919e42a30e9a4d317f969c7d4
musyokamuasya <josephmuasya8@gmail.com>
2022-09-24 22:48:29 +0300

    Base bottom sheet adapter class

--- app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseBottomSheet.kt
+++ app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseBottomSheet.kt
@@ -1,4 +1,40 @@
 package dev.ciox.ratiba.features.shared.abstracts
 
-class BaseBottomSheet {
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dev.ciox.ratiba.R
+
+abstract class BaseBottomSheet(private val manager: FragmentManager) : BottomSheetDialogFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dialog?.setOnShowListener {
+            val bottomSheetDialog = dialog as BottomSheetDialog
+            val bottomSheet =
+                bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+            with(BottomSheetBehavior.from(bottomSheet)) {
+                state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        BottomSheetDialog(requireContext(), theme)
+
+    fun show() {
+        if (!this.isAdded || !this.isVisible)
+            show(manager, this::class.java.name)
+    }
+
+    inline fun show(sheet: BaseBottomSheet.() -> Unit) {
+        this.sheet()
+        this.show()
+    }
+
 }
