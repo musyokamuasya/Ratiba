0255d81bdc2147ad747ed932611b283531b36fe6
musyokamuasya <josephmuasya8@gmail.com>
2022-09-24 22:45:45 +0300

    Base basic adapter class implementation

--- app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseBasicAdapter.kt
+++ app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseBasicAdapter.kt
@@ -1,4 +1,26 @@
 package dev.ciox.ratiba.features.shared.abstracts
 
-class BaseBasicAdapter {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseBasicAdapter<T, VH : BaseBasicAdapter.BaseBasicViewHolder<T>>
+    : RecyclerView.Adapter<VH>() {
+
+    protected val items = arrayListOf<T>()
+
+    fun submitList(list: List<T>) {
+        items.clear()
+        items.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    interface ActionListener<T> {
+        fun onActionPerformed(t: T, position: Int, action: Action)
+
+        enum class Action { SELECT, DELETE }
+    }
+
+    abstract class BaseBasicViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        abstract fun onBind(t: T, position: Int)
+    }
 }
