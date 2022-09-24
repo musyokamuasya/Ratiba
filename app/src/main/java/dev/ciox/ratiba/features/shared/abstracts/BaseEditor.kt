3d3e4feb0bf475ead6d5f1480f683d4f172530b5
musyokamuasya <josephmuasya8@gmail.com>
2022-09-24 22:53:36 +0300

    Base editor class implementation

--- app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseEditor.kt
+++ app/src/main/java/dev/ciox/ratiba/features/shared/abstracts/BaseEditor.kt
@@ -1,8 +1,14 @@
 package dev.ciox.ratiba.features.shared.abstracts
 
 import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowInsetsController
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import dev.ciox.ratiba.R
 
 abstract class BaseEditor : BaseFragment() {
 
